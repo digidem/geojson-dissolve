aae1e4f0d59daaae7efe08c778931eb06554cf78
Gregor MacLennan <gmaclennan@digital-democracy.org>
2017-04-11 20:37:02 -0700

    Check fixures are valid GeoJSON, correct winding order etc.

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var test = require('tape')
 var glob = require('glob')
 var fs = require('fs')
 var path = require('path')
+var hint = require('@mapbox/geojsonhint').hint
 
 var dissolve = require('../')
 
@@ -13,8 +14,23 @@ test('geojson-dissolve', function (t) {
   glob.sync(pattern).forEach(function (input) {
     var geojson = JSON.parse(fs.readFileSync(input))
     var output = dissolve(geojson)
-    if (REGEN) fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output, null, 2))
-    t.deepEqual(output, JSON.parse(fs.readFileSync(input.replace('/in/', '/out/'))), input)
+    if (REGEN) {
+      var inputErrors = hint(geojson)
+      var outputErrors = hint(output)
+      if (!inputErrors.length && !outputErrors.length) {
+        fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output, null, 2))
+      } else {
+        var relativePath = path.relative(__dirname, input)
+        inputErrors.forEach(onError.bind(null, relativePath))
+        outputErrors.forEach(onError.bind(null, 'generated output for ' + relativePath))
+      }
+    } else {
+      t.deepEqual(output, JSON.parse(fs.readFileSync(input.replace('/in/', '/out/'))), input)
+    }
   })
   t.end()
+
+  function onError (filename, e) {
+    t.fail('invalid GeoJSON: ' + filename + ' - ' + e.message + ' - ' + (e.level || 'error'))
+  }
 })
